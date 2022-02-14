d913352d5577042cdbbeadacee8f33bd27ec82a2
ashrafiabir01 <ashrafiabir04@gmail.com>
2022-02-14 20:51:10 +0600

    Basic Cpp

---
+++ comments.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace  std;
+int main(){
+    //Single Line Comment
+    cout<< "Single Line Comment";
+    /* Multiple Line Comment .
+    Hi I'm Ashrafi Abir . What's your name? */
+    cout << "Multiple Line Comment";
+
+}
