7c0cacfed5e1f22ffc5972513e4ae85ba9ed13c3
ashrafiabir01 <ashrafiabir04@gmail.com>
2022-02-14 21:06:43 +0600

    Add Data types examples:")

---
+++ datatype_example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // declare variable
+string name;
+int age;
+float result;
+double gpa;
+bool sad;
+char c;
+ // init variable
+ name = "Ashrafi Abir";
+ result = 14332.4;
+ gpa = 34.34343434;
+ sad = false;
+ c = 'c';
+
+}
